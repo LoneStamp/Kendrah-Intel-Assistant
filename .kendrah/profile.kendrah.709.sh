b54bf5f2e728c07d08d9a94748289f3591168fed
BlackRose <nissandiezonencaboagripoz9@gmail.com>
2025-03-16 22:55:13 +0800

    Create profile.kendrah.709.sh

---
+++ .kendrah/profile.kendrah.709.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Kendrah.#709
+NAME="Kendrah"
+AGE=22
+STATUS="Virgin"
+NATIONALITY="Russian"
+CITY="Belorechensk"
+UNIVERSITY="Adgate University"
+FAVORITE_COLOR="Black"
+
+echo "Profile Information:"
+echo "Name: $NAME"
+echo "Age: $AGE"
+echo "Status: $STATUS"
+echo "Nationality: $NATIONALITY"
+echo "City: $CITY"
+echo "University: $UNIVERSITY"
+echo "Favorite Color: $FAVORITE_COLOR"
